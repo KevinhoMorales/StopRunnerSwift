1c119fc04478f6adbccb8376082eefb0d9993103
Kevin Morales <kmorales@technisys.com>
2021-09-28 12:08:21 -0500

    Init commit

---
+++ StopRunnerSwift WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StopRunnerSwift WatchKit Extension
+//
+//  Created by Kevin Morales on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StopRunnerSwift WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  StopRunnerSwift WatchKit Extension
+//
+//  Created by Kevin Morales on 9/28/21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
